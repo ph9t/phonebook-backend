772d182ffb5c20e8ea62cadfba1e24a2ee362ae2
Angel Jimenez <abruce.jimenez@gmail.com>
2023-03-19 16:10:52 +0800

    Fix route controller error

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
-        res.json(response)
+        res.json(person)
       } else {
         res.status(404).end()
       }
